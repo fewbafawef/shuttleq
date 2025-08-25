559fbbabbfe3d774521b18a7729bb7cc58392965
fewbafawef <88759433+fewbafawef@users.noreply.github.com>
2025-08-25 07:53:12 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,10 @@
+#to do: rest of the project
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    app.run()
